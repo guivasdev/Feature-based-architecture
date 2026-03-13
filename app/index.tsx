341bb2cbfc0b2398466218978ee1c592ca207ec1
Guilherme V.Baldesin <guivasbaldesin@gmail.com>
2026-03-13 13:39:49 -0300

    estrutura finalizada

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function login() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red' }}>
+        </View>
+    )
+}
+//titulo
+//form
